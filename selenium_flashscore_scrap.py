d78323a1501fba624475ea1ed06472dfbe128efa
Anghel Mihnea <32762329+kappaq@users.noreply.github.com>
2022-11-24 05:28:08 +0200

    update

--- selenium_flashscore_scrap.py
+++ selenium_flashscore_scrap.py
@@ -7,6 +7,13 @@ from collections import defaultdict
 import pandas as pd 
 import time
 
+games = ['//*[@id="g_1_jsidfnm5"]','//*[@id="g_1_0G0LfGbo"]','//*[@id="g_1_4pe0g62B"]','//*[@id="g_1_GUm04257"]',
+'//*[@id="g_1_p42XiEr4"]', '//*[@id="g_1_8pCMVNR1"]', '//*[@id="g_1_2ucxiYcA"]', '//*[@id="g_1_IJ781Sp9"]',
+'//*[@id="g_1_ARmio3Rl"]', '//*[@id="g_1_YXjQDWEJ"]', '//*[@id="g_1_hjgnnqCr"]']
+
+lista_category = []
+n = 0
+
 dict_res = defaultdict(list)
 
 PATH = 'C:\\Users\\anghe\\Desktop\\Challange WC\\chromedriver.exe'
@@ -16,36 +23,50 @@ a = ActionChains(driver)
 wait = WebDriverWait(driver, 10)
 
 driver.get('https://www.flashscore.com/')
-#driver.fullscreen_window()
-time.sleep(5)
+time.sleep(1)
+
 accept_cookie_main_page = driver.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')
 driver.execute_script("arguments[0].click();", accept_cookie_main_page)
-time.sleep(5)
+time.sleep(1)
+
 world_cup = driver.find_element(By.XPATH, '//*[@id="my-leagues-list"]/div[3]/div[2]/a')
-driver.implicitly_wait(5)
+driver.implicitly_wait(1)
 world_cup.click()
-time.sleep(5)
-#driver.fullscreen_window()
-original_window = driver.current_window_handle
-time.sleep(5)
-game_1 = driver.find_element(By.XPATH, '//*[@id="g_1_ARmio3Rl"]')
-driver.execute_script("arguments[0].click();", game_1)
-time.sleep(5)
-wait.until(EC.number_of_windows_to_be(2))
-for window_handle in driver.window_handles:
-        if window_handle != original_window:
-            driver.switch_to.window(window_handle)
-            break
-stats = driver.find_element(By.XPATH, '//*[@id="detail"]/div[7]/div/a[2]')
-driver.execute_script("arguments[0].click();", stats)
-time.sleep(5)
-time = driver.find_element(By.XPATH, '//*[@id="detail"]/div[5]/div[1]')
-home_team_name = driver.find_element(By.XPATH, '//*[@id="detail"]/div[5]/div[2]')
-away_team_name = driver.find_element(By.XPATH, '//*[@id="detail"]/div[5]/div[4]')
-final_score = driver.find_element(By.XPATH, '//*[@id="detail"]/div[1]/div[2]/div/div[1]')
-dict_res['time and date'].append(time.text)
-dict_res['game'].append(home_team_name.text + '-' + away_team_name.text)
-dict_res['score'].append(final_score.text)
+time.sleep(1)
+
+results = driver.find_element(By.XPATH, '//*[@id="li1"]')
+driver.execute_script("arguments[0].click();", results)
+
+
+for game in games:
+	original_window = driver.current_window_handle
+	game_1 = driver.find_element(By.XPATH, game)
+	driver.execute_script("arguments[0].click();", game_1)
+	wait.until(EC.number_of_windows_to_be(2))
+	for window_handle in driver.window_handles:
+	        if window_handle != original_window:
+	            driver.switch_to.window(window_handle)
+	            break
+	stats = driver.find_element(By.XPATH, '//*[@id="detail"]/div[7]/div/a[2]')
+	driver.execute_script("arguments[0].click();", stats)
+	section = driver.find_elements(By.CLASS_NAME, ('stat__row'))
+	for a in section:
+		lista_category.append(a)
+		split_list = lista_category[n].text.split()
+		n = n+1
+		lista_category_final_s = ' '.join(map(str, split_list[1:-1]))
+		lista_home = ''.join(map(str, split_list[0]))
+		lista_away = ''.join(map(str, split_list[-1]))
+		dict_res[lista_category_final_s].append(lista_home + "-" + lista_away)
+	time = driver.find_element(By.XPATH, '//*[@id="detail"]/div[5]/div[1]')
+	home_team_name = driver.find_element(By.XPATH, '//*[@id="detail"]/div[5]/div[2]')
+	away_team_name = driver.find_element(By.XPATH, '//*[@id="detail"]/div[5]/div[4]')
+	final_score = driver.find_element(By.XPATH, '//*[@id="detail"]/div[5]/div[3]/div/div[1]')
+	dict_res['Time and Date'].append(time.text)
+	dict_res['Game'].append(home_team_name.text + '-' + away_team_name.text)
+	dict_res['Final Score'].append(final_score.text.split()[0] + '-' + final_score.text.split()[-1])
+	driver.close()
+	driver.switch_to.window(original_window)
 df_res = pd.DataFrame(dict_res)
 print("--------")
 print("\n")
