5ed61a106f0ffec15bd76c5ce7f8884eda2f5a85
anamachidon <64096216+anamachidon@users.noreply.github.com>
2023-02-20 17:41:19 +0200

    Add files via upload

---
+++ airflow/dag/who_scored_player_detailed_goal_dag.py
@@ -0,0 +1,18 @@
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+args = {
+    'owner': 'kappaq',
+    'start_date': days_ago(1)
+}
+
+dag = DAG(
+    dag_id='who_scored_player_detailed_goal_scrap-dag',
+    default_args=args,
+    schedule_interval='@daily'
+)
+
+the_analyst_scrap = BashOperator(task_id="who_scored_player_detailed_goal_scrap",
+                                 bash_command='python /opt/airflow/dags/script/who_scored_player_detailed_goal_scrap.py',
+                                 dag=dag)
