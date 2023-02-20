7b11fbbe04a04e13a4533f845e37c9a0d4c7f9da
anamachidon <64096216+anamachidon@users.noreply.github.com>
2023-02-20 12:48:40 +0200

    Add files via upload

---
+++ airflow/dag/ranking_dag.py
@@ -0,0 +1,18 @@
+from airflow.models import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+args = {
+    'owner': 'kappaq',
+    'start_date': days_ago(1)
+}
+
+dag = DAG(
+    dag_id='who_scored_team-dag',
+    default_args=args,
+    schedule_interval='@daily'
+)
+
+who_scored_team_scrap = BashOperator(task_id="who_scored_team_scrap",
+                                     bash_command='python /opt/airflow/dags/script/ranking_scrap.py',
+                                     dag=dag)
